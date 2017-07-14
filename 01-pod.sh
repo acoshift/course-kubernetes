d0fc4dc1411f8fff6f138220b9f6d6a24feb5652
acoshift <acoshift@gmail.com>
2017-07-14 15:42:06 +0700

    init

---
+++ 01-pod.sh
@@ -0,0 +1,38 @@
+$ kubectl create -f 01-pod.yaml
+pod "echoserver" created
+
+$ kubectl get pods
+NAME              READY     STATUS    RESTARTS   AGE
+echoserver        1/1       Running   0          4m
+
+$ kubectl port-forward echoserver 9000:8080
+Forwarding from 127.0.0.1:9000 -> 8080
+Forwarding from [::1]:9000 -> 8080
+
+$ curl localhost:9000
+Hostname: echoserver
+
+Pod Information:
+	-no pod information available-
+
+Server values:
+	server_version=nginx: 1.13.1 - lua: 10008
+
+Request Information:
+	client_address=127.0.0.1
+	method=GET
+	real path=/
+	query=
+	request_version=1.1
+	request_uri=http://localhost:8080/
+
+Request Headers:
+	accept=*/*
+	host=localhost:9000
+	user-agent=curl/7.51.0
+
+Request Body:
+	-no body in request-
+
+$ kubectl delete pod echoserver
+pod "echoserver" deleted
---
+++ 02-service.sh
@@ -0,0 +1,12 @@
+$ kubectl create -f 02-service.yaml
+pod "echoserver" created
+service "echoserver" created
+
+$ kubectl get services
+NAME         CLUSTER-IP   EXTERNAL-IP   PORT(S)   AGE
+echoserver   10.3.248.15  <none>        80/TCP    11s
+
+$ kubectl run -i -t --rm busybox --image=busybox
+$ wget -O - http://echoserver
+
+$ kubectl delete -f 02-service.yaml
