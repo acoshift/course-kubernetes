f23d1bfddce46c92a64da4d7daef39a004a2acb7
acoshift <acoshift@gmail.com>
2017-07-16 01:26:59 +0700

    add 21

---
+++ 21.sh
@@ -0,0 +1,11 @@
+$ kubectl create -f 21-cm.yaml
+configmap "redis-config" created
+service "redis" created
+statefulset "redis" created
+
+$ kubectl run -it --rm redis-client --image=redis --restart=Never --command -- bash
+root@redis-client:/data# redis-cli -h redis
+
+redis:6379> config get save
+1) "save"
+2) ""
