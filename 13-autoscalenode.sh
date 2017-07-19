b5c99d7565e1ae3b5b74179a53427df6399d396a
acoshift <acoshift@gmail.com>
2017-07-19 18:52:01 +0700

    refactor

--- 13-autoscalenode.sh
+++ 13-autoscalenode.sh
@@ -3,4 +3,5 @@ $ gcloud alpha container clusters update cluster-1 \
   --min-nodes=2 \
   --max-nodes=6 \
   --zone=asia-southeast1-b \
-  --node-pool=default-pool
+  --node-pool=default-pool \
+  --project=acoshift-k8s
