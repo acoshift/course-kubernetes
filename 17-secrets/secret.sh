ee06995cf1f758c47e57413d648e580d00583e2b
acoshift <acoshift@gmail.com>
2017-10-08 14:18:34 +0700

    change domain

--- 17-secrets/secret.sh
+++ 17-secrets/secret.sh
@@ -4,12 +4,12 @@ bXlzcWxwYXNzd29yZA==
 $ kubectl create -f secret.yaml
 
 
-$ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=echoserver.acoshift.com"
+$ openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout tls.key -out tls.crt -subj "/CN=echoserver-secure.acoshift.me"
 Generating a 2048 bit RSA private key
 .....................................+++
 ...+++
 writing new private key to 'tls.key'
 -----
 
-$ kubectl create secret tls echoserver-acoshift-com-tls --key=tls.key --cert=tls.crt
-secret "echoserver-acoshift-com-tls" created
+$ kubectl create secret tls echoserver-secure-acoshift-me-tls --key=tls.key --cert=tls.crt
+secret "echoserver-secure-acoshift-me-tls" created
